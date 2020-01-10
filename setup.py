253588c8801eddaa943cc09e553b282ec51a066b
Work Prahl <prahl@bme.ogi.edu>
2020-01-09 16:01:11 -0800

    update version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from setuptools import setup
 setup(
 	name='RigolWFM',
 	packages=['RigolWFM'],
-	version='0.1.0',
+	version='0.1.1',
 	description='Read and parse Rigol Oscilloscope WFM files',
 	url='https://github.com/scottprahl/rigol_fm.git',  
 	author='Scott Prahl',
