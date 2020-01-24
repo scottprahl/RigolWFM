failing to fix voltage scaling

RigolWFM/wfme.py
@@ -57,7 +57,7 @@ class Channel():
         self.time_delta = 0.1  # s
         self.time_offset = 0   # s
         self.points = 0
-        self.raw = np.array(0)
+        self.raw = None
 
     def __str__(self):
         s = "Channel %d\n" % self.channel_number
@@ -67,13 +67,15 @@ class Channel():
         s += "        Offset = " + engineering_string(self.volt_offset) + "V\n"
         s += "    Time:\n"
         s += "        Scale  = " + engineering_string(self.time_scale) + "s/div\n"
-        s += "        Offset = " + engineering_string(self.time_offset) + "s\n"
-        s += "        Delta  = " + engineering_string(self.time_delta) + "s\n"
+        s += "        Delay  = " + engineering_string(self.time_offset) + "s\n"
+        s += "        Delta  = " + engineering_string(self.time_delta) + "s/point\n"
         s += "    Raw volts:\n"
         s += "        Points = %d\n" % self.points
-        if self.raw:
-            s += "        [%d,%d,%d,...,%d,%d]\n" % (
+        if self.enabled:
+            s += "        Raw   = [%d,%d,%d,...,%d,%d]\n" % (
                 self.raw[0], self.raw[1], self.raw[2], self.raw[-2], self.raw[-1])
+            v = [engineering_string(self.volts[i])+"V" for i in [0,1,2,-2,-1]]
+            s += "        Volts  = [%s,%s,%s,...,%s,%s]\n" % (v[0],v[1],v[2],v[3],v[4])
         return s
 
 
@@ -86,25 +88,27 @@ class ChannelE(Channel):
             self.channel_number = 1
             self.enabled = w.header.ch1.enabled
             self.volt_scale = w.header.ch1_volt_scale
-            self.volt_offset = w.header.ch1_volt_shift * w.header.ch1_volt_scale / 50
+            self.volt_offset = w.header.ch1_volt_shift/25
             self.time_delta = 1/w.header.sample_rate
             self.time_offset = w.header.ch1_time_delay
             self.time_scale = w.header.ch1_time_scale
             self.points = w.header.ch1_points
             if self.enabled:
-                self.raw = w.data.ch1
+                self.raw =np.array(w.data.ch1)
+                self.volts = self.volt_scale * self.raw/55 - self.volt_offset
                 
         elif ch == 2:
             self.channel_number = 2
             self.enabled = w.header.ch2.enabled
             self.volt_scale = w.header.ch2_volt_scale
-            self.volt_offset = w.header.ch2_volt_shift * w.header.ch2_volt_scale / 50
+            self.volt_offset = w.header.ch2_volt_shift/25
             self.time_delta = 1/w.header.sample_rate
             self.time_offset = w.header.ch2_time_delay
             self.time_scale = w.header.ch2_time_scale
             self.points = w.header.ch2_points
             if self.enabled:
-                self.raw = w.data.ch2
+                self.raw = np.array(w.data.ch2)
+                self.volts = self.volt_scale * self.raw / 55- self.volt_offset
 
 
 class ReadWFMError(Exception):