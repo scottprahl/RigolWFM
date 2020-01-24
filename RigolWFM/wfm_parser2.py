fee42d302b41b391e25b617e5f3d25a715f479a4
Scott Prahl <scott@prahls.com>
2020-01-24 12:10:08 -0800

    basic parsing framework for ksy parsers

---
+++ RigolWFM/wfm_parser2.py
@@ -0,0 +1,25 @@
+import argparse
+import wfme
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='wfm_parse',
+        description='Parse Rigol WFM files.'
+    )
+    parser.add_argument(
+        '-t',
+        choices=('c', 'd', 'e', 'z'),
+        required=True,
+        help='the type of Rigol WFM file'
+    )
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    print(args.filename)
+    
+    channels = wfme.parse(args.filename)
+    for ch in channels:
+        print(ch)
+
+
+if __name__ == "__main__":
+    main()
---
+++ RigolWFM/wfme.py
@@ -0,0 +1,131 @@
+#pylint: disable=invalid-name
+#pylint: disable=too-many-instance-attributes
+
+"""
+Extract signals or description from Rigol 1000E Oscilloscope waveform file.
+
+Use like this::
+
+    import RigolWFM.wfme as wfme
+
+    channels = wfme.parse("filename.wfm")
+    for ch in channels:
+        print(ch)
+"""
+
+import tempfile
+import requests
+import numpy as np
+
+import wfm1000e
+
+
+def engineering_string(number):
+    """Format number with proper prefix"""
+    absnr = abs(number)
+
+    if absnr == 0:
+        engr_str = '%g ' % (number / 1e-12)
+    elif absnr < 1e-9:
+        engr_str = '%g p' % (number / 1e-12)
+    elif absnr < 1e-6:
+        engr_str = '%g n' % (number / 1e-9)
+    elif absnr < 1e-3:
+        engr_str = '%g µ' % (number / 1e-6)
+    elif absnr < 1:
+        engr_str = '%g m' % (number / 1e-3)
+    elif absnr < 1e3:
+        engr_str = '%g ' % (number)
+    elif absnr < 1e6:
+        engr_str = '%g k' % (number / 1e3)
+    elif absnr < 1e9:
+        engr_str = '%g M' % (number / 1e6)
+    else:
+        engr_str = '%g G' % (number / 1e9)
+    return engr_str
+
+
+class Channel():
+    """Base class for a single channel."""
+
+    def __init__(self):
+        self.channel_number = 1
+        self.enabled = False
+        self.volt_scale = 1    # s/div
+        self.volt_offset = 0   # s
+        self.time_scale = 1    # s/div
+        self.time_delta = 0.1  # s
+        self.time_offset = 0   # s
+        self.points = 0
+        self.raw = np.array(0)
+
+    def __str__(self):
+        s = "Channel %d\n" % self.channel_number
+        s += "    Enabled: %s\n" % self.enabled
+        s += "    Voltage:\n"
+        s += "        Scale  = " + engineering_string(self.volt_scale) + "V/div\n"
+        s += "        Offset = " + engineering_string(self.volt_offset) + "V\n"
+        s += "    Time:\n"
+        s += "        Scale  = " + engineering_string(self.time_scale) + "s/div\n"
+        s += "        Offset = " + engineering_string(self.time_offset) + "s\n"
+        s += "        Delta  = " + engineering_string(self.time_delta) + "s\n"
+        s += "    Raw volts:\n"
+        s += "        Points = %d\n" % self.points
+        if self.raw:
+            s += "        [%d,%d,%d,...,%d,%d]\n" % (
+                self.raw[0], self.raw[1], self.raw[2], self.raw[-2], self.raw[-1])
+        return s
+
+
+class ChannelE(Channel):
+    """Base class for a single channel from 1000E series scopes."""
+
+    def __init__(self, w, ch=1):
+        super().__init__()
+        if ch == 1:
+            self.channel_number = 1
+            self.enabled = w.header.ch1.enabled
+            self.volt_scale = w.header.ch1_volt_scale
+            self.volt_offset = w.header.ch1_volt_shift * w.header.ch1_volt_scale / 50
+            self.time_delta = 1/w.header.sample_rate
+            self.time_offset = w.header.ch1_time_delay
+            self.time_scale = w.header.ch1_time_scale
+            self.points = w.header.ch1_points
+            if self.enabled:
+                self.raw = w.data.ch1
+                
+        elif ch == 2:
+            self.channel_number = 2
+            self.enabled = w.header.ch2.enabled
+            self.volt_scale = w.header.ch2_volt_scale
+            self.volt_offset = w.header.ch2_volt_shift * w.header.ch2_volt_scale / 50
+            self.time_delta = 1/w.header.sample_rate
+            self.time_offset = w.header.ch2_time_delay
+            self.time_scale = w.header.ch2_time_scale
+            self.points = w.header.ch2_points
+            if self.enabled:
+                self.raw = w.data.ch2
+
+
+class ReadWFMError(Exception):
+    """Generic Read Error."""
+
+
+class ParseWFMError(Exception):
+    """Generic Parse Error."""
+
+
+def parse(wfm_filename):
+    """Return a list of channels."""
+
+    channels = [None, None]
+
+    try:
+        w = wfm1000e.Wfm1000e.from_file(wfm_filename)
+        channels = [ChannelE(w, i) for i in [1,2]]
+        return channels
+
+    except:
+        raise ParseWFMError("File format is not 1000E.  Sorry.")
+
+    return channels
