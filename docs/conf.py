78d3b2b65b3149399a752b0da1566060b6411172
Scott Prahl <scott@prahls.com>
2020-03-28 19:33:28 -0700

    readthedocs has its own ideas

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ author = 'Scott Prahl'
 # The full version, including alpha/beta/rc tags
 release = '0.6.3'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
