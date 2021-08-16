0aa8950a2401b4136e22277d7bafe57a56497bfc
Scott Prahl <scott@prahls.com>
2021-08-16 10:09:59 -0700

    revert to rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ source_suffix = ['.rst', '.ipynb']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
 
 #html_theme = 'sphinx_rtd_theme'
 
