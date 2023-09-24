2c31479e6217981e1872d8c22e6aaafdd8bcee70
Scott Prahl <scott@prahls.com>
2023-09-24 08:57:51 -0700

    fix testing

--- tests/test_wfmconvert.py
+++ tests/test_wfmconvert.py
@@ -75,3 +75,13 @@ def test_wfmconvert_sigrok():
     for command in commands:
         result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         assert result.returncode == 0
+
+# Run the tests
+if __name__ == "__main__":
+    test_wfmconvert_info()
+    test_wfmconvert_csv()
+    test_wfmconvert_wav()
+    test_wfmconvert_vcsv()
+    test_wfmconvert_sigrok()
+    print("All tests passed!")
+
