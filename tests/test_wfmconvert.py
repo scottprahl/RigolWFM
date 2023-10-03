90d8068f2be0eb03521c9cf3b542d77c186f826d
Scott Prahl <scott@prahls.com>
2023-10-03 16:11:04 -0700

    debugging action script

--- tests/test_wfmconvert.py
+++ tests/test_wfmconvert.py
@@ -3,13 +3,13 @@ import subprocess
 
 def test_wfmconvert_info():
     commands = [
-        "wfmconvert B info ../tests/DS1204B-A.wfm",
-        "wfmconvert C info ../tests/DS1202CA-A.wfm",
-        "wfmconvert D info ../tests/DS1102D-A.wfm",
-        "wfmconvert E info ../tests/DS1102E-A.wfm",
-        "wfmconvert Z info ../tests/MSO1104.wfm",
-        "wfmconvert 2 info ../tests/DS2202.wfm",
-        "wfmconvert 4 info ../tests/DS4022-A.wfm"
+        "wfmconvert B info tests/DS1204B-A.wfm",
+        "wfmconvert C info tests/DS1202CA-A.wfm",
+        "wfmconvert D info tests/DS1102D-A.wfm",
+        "wfmconvert E info tests/DS1102E-A.wfm",
+        "wfmconvert Z info tests/MSO1104.wfm",
+        "wfmconvert 2 info tests/DS2202.wfm",
+        "wfmconvert 4 info tests/DS4022-A.wfm"
     ]
 
     for command in commands:
@@ -18,13 +18,13 @@ def test_wfmconvert_info():
 
 def test_wfmconvert_csv():
     commands = [
-        "wfmconvert --force B csv ../tests/DS1204B-A.wfm",
-        "wfmconvert --force C csv ../tests/DS1202CA-A.wfm",
-        "wfmconvert --force D csv ../tests/DS1102D-A.wfm",
-        "wfmconvert --force E csv ../tests/DS1102E-A.wfm",
-        "wfmconvert --force Z csv ../tests/MSO1104.wfm",
-        "wfmconvert --force 2 csv ../tests/DS2202.wfm",
-        "wfmconvert --force 4 csv ../tests/DS4022-A.wfm"
+        "wfmconvert --force B csv tests/DS1204B-A.wfm",
+        "wfmconvert --force C csv tests/DS1202CA-A.wfm",
+        "wfmconvert --force D csv tests/DS1102D-A.wfm",
+        "wfmconvert --force E csv tests/DS1102E-A.wfm",
+        "wfmconvert --force Z csv tests/MSO1104.wfm",
+        "wfmconvert --force 2 csv tests/DS2202.wfm",
+        "wfmconvert --force 4 csv tests/DS4022-A.wfm"
     ]
 
     for command in commands:
@@ -33,13 +33,13 @@ def test_wfmconvert_csv():
 
 def test_wfmconvert_wav():
     commands = [
-        "wfmconvert --force B wav ../tests/DS1204B-A.wfm",
-        "wfmconvert --force C wav ../tests/DS1202CA-A.wfm",
-        "wfmconvert --force D wav ../tests/DS1102D-A.wfm",
-        "wfmconvert --force E wav ../tests/DS1102E-A.wfm",
-        "wfmconvert --force Z wav ../tests/MSO1104.wfm",
-        "wfmconvert --force 2 wav ../tests/DS2202.wfm",
-        "wfmconvert --force 4 wav ../tests/DS4022-A.wfm"
+        "wfmconvert --force B wav tests/DS1204B-A.wfm",
+        "wfmconvert --force C wav tests/DS1202CA-A.wfm",
+        "wfmconvert --force D wav tests/DS1102D-A.wfm",
+        "wfmconvert --force E wav tests/DS1102E-A.wfm",
+        "wfmconvert --force Z wav tests/MSO1104.wfm",
+        "wfmconvert --force 2 wav tests/DS2202.wfm",
+        "wfmconvert --force 4 wav tests/DS4022-A.wfm"
     ]
 
     for command in commands:
@@ -48,13 +48,13 @@ def test_wfmconvert_wav():
 
 def test_wfmconvert_vcsv():
     commands = [
-        "wfmconvert --force B vcsv ../tests/DS1204B-A.wfm",
-        "wfmconvert --force C vcsv ../tests/DS1202CA-A.wfm",
-        "wfmconvert --force D vcsv ../tests/DS1102D-A.wfm",
-        "wfmconvert --force E vcsv ../tests/DS1102E-A.wfm",
-        "wfmconvert --force Z vcsv ../tests/MSO1104.wfm",
-        "wfmconvert --force 2 vcsv ../tests/DS2202.wfm",
-        "wfmconvert --force 4 vcsv ../tests/DS4022-A.wfm"
+        "wfmconvert --force B vcsv tests/DS1204B-A.wfm",
+        "wfmconvert --force C vcsv tests/DS1202CA-A.wfm",
+        "wfmconvert --force D vcsv tests/DS1102D-A.wfm",
+        "wfmconvert --force E vcsv tests/DS1102E-A.wfm",
+        "wfmconvert --force Z vcsv tests/MSO1104.wfm",
+        "wfmconvert --force 2 vcsv tests/DS2202.wfm",
+        "wfmconvert --force 4 vcsv tests/DS4022-A.wfm"
     ]
 
     for command in commands:
@@ -63,13 +63,13 @@ def test_wfmconvert_vcsv():
 
 def test_wfmconvert_sigrok():
     commands = [
-        "wfmconvert --force B sigrok ../tests/DS1204B-A.wfm",
-        "wfmconvert --force C sigrok ../tests/DS1202CA-A.wfm",
-        "wfmconvert --force D sigrok ../tests/DS1102D-A.wfm",
-        "wfmconvert --force E sigrok ../tests/DS1102E-A.wfm",
-        "wfmconvert --force Z sigrok ../tests/MSO1104.wfm",
-        "wfmconvert --force 2 sigrok ../tests/DS2202.wfm",
-        "wfmconvert --force 4 sigrok ../tests/DS4022-A.wfm"
+        "wfmconvert --force B sigrok tests/DS1204B-A.wfm",
+        "wfmconvert --force C sigrok tests/DS1202CA-A.wfm",
+        "wfmconvert --force D sigrok tests/DS1102D-A.wfm",
+        "wfmconvert --force E sigrok tests/DS1102E-A.wfm",
+        "wfmconvert --force Z sigrok tests/MSO1104.wfm",
+        "wfmconvert --force 2 sigrok tests/DS2202.wfm",
+        "wfmconvert --force 4 sigrok tests/DS4022-A.wfm"
     ]
 
     for command in commands:
