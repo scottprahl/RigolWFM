8712a37aaf6576b386758335ac2d7bb91d08f961
Scott Prahl <scott@prahls.com>
2023-10-05 13:13:34 -0700

    eliminate most lfs data use

--- tests/test_wfmconvert_sigrok.py
+++ tests/test_wfmconvert_sigrok.py
@@ -2,13 +2,13 @@ import subprocess
 
 def test_wfmconvert_sigrok():
     commands = [
-        "wfmconvert --force B sigrok tests/DS1204B-A.wfm",
-        "wfmconvert --force C sigrok tests/DS1202CA-A.wfm",
+#        "wfmconvert --force B sigrok tests/DS1204B-A.wfm",
+#        "wfmconvert --force C sigrok tests/DS1202CA-A.wfm",
         "wfmconvert --force D sigrok tests/DS1102D-A.wfm",
-        "wfmconvert --force E sigrok tests/DS1102E-A.wfm",
-        "wfmconvert --force Z sigrok tests/MSO1104.wfm",
-        "wfmconvert --force 2 sigrok tests/DS2202.wfm",
-        "wfmconvert --force 4 sigrok tests/DS4022-A.wfm"
+#        "wfmconvert --force E sigrok tests/DS1102E-A.wfm",
+#        "wfmconvert --force Z sigrok tests/MSO1104.wfm",
+#        "wfmconvert --force 2 sigrok tests/DS2202.wfm",
+#        "wfmconvert --force 4 sigrok tests/DS4022-A.wfm"
     ]
 
     for command in commands:
